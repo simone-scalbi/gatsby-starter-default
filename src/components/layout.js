e2e57f53d9d04880fd1ee1d3953a7c41efa10512
Simone Scalbi <simone.scalbi@wisemotions.com>
2022-02-24 14:52:20 +0100

    feat: test 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,7 +39,7 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with SIMO 1 -{` `}
+          © {new Date().getFullYear()}, Built with SIMO 2 -{` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
       </div>
